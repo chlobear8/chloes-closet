fcaca3d58e25db0dbca6552fb5bc1891fba6eded
Chloe ONeil <chloesporri@yahoo.com>
2023-06-29 09:42:14 -0700

    add Closet.js and boiler

---
+++ src/components/Closet.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Closet() {
+  return (
+    <React.Fragment>
+      <h3>Category</h3>
+      <h3>Type</h3>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Ticket;
