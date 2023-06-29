39821a409a94b8ba431b7f2294e38f4c8ede2864
Chloe ONeil <chloesporri@yahoo.com>
2023-06-29 10:18:03 -0700

    add if else statement for basic form handling

--- src/components/ClosetControl.js
+++ src/components/ClosetControl.js
@@ -2,6 +2,29 @@ import React from "react";
 import NewClosetForm from "./NewClosetForm";
 import ClosetList from "./ClosetList";
 
-class ClosetControl extends React.Component {
-  
+function ClosetControl () {
+
+  const handleClick = () => {
+    setFormVisibleOnPage(true);
+  }
+
+  let currentlyVisibleState = null;
+  let addClothingButton = null;
+
+  if (formVisibleOnPage) {
+    currentlyVisibleState = <NewClosetForm onNewClosetCreation = {handleAddingNewArticleToList} />;
+    buttonText = "Return to Closet";
+  } else {
+    currentlyVisibleState = <ClosetList onClothingSelection = {handleChangingSelection} closetList = {mainClosetList} />;
+    buttonText = "Add Clothing";
+  }
+
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+      <button onClick = {handleClick}>{buttonText}</button>
+    </React.Fragment>
+  );
 }
+
+export default ClosetControl;
