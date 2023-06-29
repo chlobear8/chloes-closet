0d6439d1f717b53ea9eadd530de998f5b728a99a
Chloe ONeil <chloesporri@yahoo.com>
2023-06-29 09:49:41 -0700

    add props to Closet

--- src/components/Closet.js
+++ src/components/Closet.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Closet() {
+function Closet(props) {
   return (
     <React.Fragment>
-      <h3>Category</h3>
-      <h3>Type</h3>
+      <h3>{props.category}</h3>
+      <h3>{props.type}</h3>
       <hr/>
     </React.Fragment>
   );
