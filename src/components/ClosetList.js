5e76cb94c57db33e115ae82f5a7d57626c3a7e30
Chloe ONeil <chloesporri@yahoo.com>
2023-06-29 17:23:17 -0700

    add propTypes to ClosetList and refactor

--- src/components/ClosetList.js
+++ src/components/ClosetList.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Closet from "./Closet";
+import PropTypes from "prop-types";
 
-function ClosetList() {
+function ClosetList(props) {
   return (
     <React.Fragment>
+      <hr />
+      {props.articleList.map((article) =>
       <Closet 
-        articleOfClothing= "name"
-        category= {[]}
-        occasion= {[]}/>
+      whenArticleClicked={props.onArticleSelection}
+      articleOfClothing= {article.articleOfClothing}
+      category= {article.category}
+      occasion= {article.occassion}
+      id= {article.id}
+      key= {article.id} />
+      )}
     </React.Fragment>
   );
+}
+
+ClosetList.propTypes = {
+  closetList: PropTypes.array,
+  onArticleSelection: PropTypes.func
 }
 
 export default ClosetList;
