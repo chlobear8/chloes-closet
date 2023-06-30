24a438de20be543afbbfd7b9a92298eb94da3243
Chloe ONeil <chloesporri@yahoo.com>
2023-06-29 17:29:27 -0700

    add ArticleDetail

---
+++ src/components/ArticleDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ArticleDetail(props) {
+  const { article, onClickingDelete, onClickingEdit } = props;
+  
+  return (
+    <React.Fragment>
+      <h1>Article's Detail</h1>
+      <h3>{article.articleOfClothing}</h3>
+      <h3>{article.category} - {article.occasion}</h3>
+      <button onClick = {() => onClickingEdit(article.id) }>Update Article</button>
+      <button onClick={() => onClickingDelete(article.id) }>Delete Article</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+ArticleDetail.propTypes = {
+  article: PropTypes.object,
+  onClickingEdit: PropTypes.func,
+  onClickingDelete: PropTypes.func
+}
+
+export default ArticleDetail;
