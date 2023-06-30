e708cc568ab42f0adf1179615e0f075aafd05ed6
Chloe ONeil <chloesporri@yahoo.com>
2023-06-29 17:18:08 -0700

    add whenArticleClicked func to Closet

--- src/components/Closet.js
+++ src/components/Closet.js
@@ -4,10 +4,12 @@ import PropTypes from "prop-types";
 function Closet(props) {
   return (
     <React.Fragment>
-      <h1>{props.articleOfClothing}</h1>
-      <h3>{props.category}</h3>
-      <h3>{props.occasion}</h3>
-      <hr/>
+      <div onClick = {() => props.whenArticleClicked(props.id)}>
+        <h1>{props.articleOfClothing}</h1>
+        <h3>{props.category}</h3>
+        <h3>{props.occasion}</h3>
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -15,7 +17,9 @@ function Closet(props) {
 Closet.propTypes = {
   articleOfClothing: PropTypes.string,
   category: PropTypes.array,
-  type: PropTypes.array
+  type: PropTypes.array,
+  id: PropTypes.string,
+  whenArticleClicked: PropTypes.func
 }
 
 export default Closet;
