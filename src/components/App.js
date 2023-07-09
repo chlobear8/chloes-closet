69f8c6d3979eb4f0f24893fe7889c9373407a55d
Chloe ONeil <chloesporri@yahoo.com>
2023-07-09 14:09:34 -0700

    WIP adding image layering to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import Header from './Header';
 import './App.css';
 import ClosetControl from './ClosetControl';
+import ImageLayer from './ImageLayer';
 
 function App() {
+  const baseImage = '';
+  const images = [
+    {
+
+    }
+  ]
   return (
     <React.Fragment>
       <Header />
       <ClosetControl />
+      <div>
+        <ImageLayer baseImage={baseImage} images={images} />
+      </div>
     </React.Fragment>
   );
 }
---
+++ src/components/ImageLayer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const imageLayering = ({ images, baseImage }) => {
+  return (
+    <div style = {{ position: 'relative' }}>
+      <img src = {baseImage} alt = "Avatar" />
+
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image.src}
+          alt={`{$index}`}
+          style={{
+            position: 'absolute',
+            top: image.position.top,
+            left: image.position.left,
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default imageLayering;
