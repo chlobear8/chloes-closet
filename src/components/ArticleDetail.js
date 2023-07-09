fc2427de550c2708ea402b85780eba083ca0100e
Chloe ONeil <chloesporri@yahoo.com>
2023-07-09 14:48:21 -0700

    add season to ArticleDetails

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -7,8 +7,9 @@ function ArticleDetail(props) {
   return (
     <React.Fragment>
       <h1>Article's Detail</h1>
-      <h3>{article.articleOfClothing}</h3>
+      <h3>{article.articleName}</h3>
       <h3>{article.category} - {article.occasion}</h3>
+      <h3>{article.season.join(", ")}</h3>
       <button onClick = {() => onClickingEdit(article.id) }>Update Article</button>
       <button onClick={() => onClickingDelete(article.id) }>Delete Article</button>
       <hr/>
