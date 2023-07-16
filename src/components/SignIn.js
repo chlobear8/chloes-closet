26c62e3fc8f2886444ab706d547c4b0aa01691bd
Chloe ONeil <chloesporri@yahoo.com>
2023-07-16 16:16:04 -0700

    add sign in functionality to SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,22 @@
 import React from "react";
 
 function SignIn(){  
+
   return (
-    <h1>Sign In</h1>
+    <React.Fragment>
+      <h1>Sign In</h1>
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+    </React.Fragment>
   );
 }
 
