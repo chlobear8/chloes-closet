dd40a6bb2c053682596e5788abfc3816d35912ec
Chloe ONeil <chloesporri@yahoo.com>
2023-07-16 07:32:28 -0700

    add lastWorn to Avatar and set up button for worn

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ImageLayer from './ImageLayer';
+import LastWornTimer from './LastWornTimer';
 
 function Avatar(props) {
   const baseImage = '';
   const images = props.articles.map(article => article.image);
-
+  const [isActive, timer, setIsActive] = LastWornTimer();
+  
   return (
-    <ImageLayer baseImage={baseImage} images={images} />
+    <React.Fragment>
+      <ImageLayer baseImage={baseImage} images={images} />
+      {isActive ? <h1>{timer}</h1> : <h1>{props.lastWorn}</h1>}
+      <button onClick={() => setIsActive(!isActive)}>Set as worn?</button>
+    </React.Fragment>
   );
 }
 
 Avatar.propTypes = {
-  articles: PropTypes.array
+  articles: PropTypes.array,
+  lastWorn: PropTypes.string
 }
 
 export default Avatar;
