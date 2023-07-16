99a0e3210f3f4d10a4ffbe38c671f43c517fc4cc
Chloe ONeil <chloesporri@yahoo.com>
2023-07-16 16:25:09 -0700

    add signUp and error handling to SignIn.js

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+import { auth } from './../firebase.js';
+import { createUserWithEmailAndPassword  } from "firebase/auth";
 
-function SignIn(){  
+function SignIn() {
+  const [signUpSuccess, setSignUpSuccess] = useState(null);
+
+  function doSignUp(e) {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setSignUpSuccess(`You are successfully signed up, ${userCredential.user.email}!`)
+      })
+      .catch((error) => {
+        setSignUpSuccess(`There was an error signing up: ${error.message}!`)
+      });
+  }
 
   return (
     <React.Fragment>
       <h1>Sign In</h1>
+      {signUpSuccess}
       <form onSubmit={doSignUp}>
         <input
           type='text'
