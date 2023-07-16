6d33ae8805337d7c79297ce47e84255a87c68fed
Chloe ONeil <chloesporri@yahoo.com>
2023-07-16 16:08:28 -0700

    add links to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <h1>Chloe's Closet</h1>
+    <React.Fragment>
+      <h1>Chloe's Closet</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/sign-in">Sign In</Link>
+        </li>
+      </ul>
+    </React.Fragment>
   );
 }
 
