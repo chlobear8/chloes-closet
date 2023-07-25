02f0053e23a7b7e4fff9cee8fbc50835fe12df6c
Chloe ONeil <chloesporri@yahoo.com>
2023-07-25 15:04:06 -0700

    add left and right scroll and add button to bottom of code

--- src/components/ImageLayer.js
+++ src/components/ImageLayer.js
@@ -4,6 +4,7 @@ function ImageLayer(props) {
 
   const { images, baseImage } = props;
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [displayedIndex, setDisplayedIndex] = useState(0);
 
   const handleImageClick = (e, image) => {
     console.log("Image clicked", image.src);
@@ -11,27 +12,46 @@ function ImageLayer(props) {
 
   const filterImagesByCategory = (category) => {
     setSelectedCategory(category);
+    setDisplayedIndex(0);
   };
 
   const handleAddImageToLayer = (image) => {
     console.log("Adding image to layer", image.src);
   };
 
+  const handleScrollLeft = () => {
+    if (displayedIndex > 0) {
+      setDisplayedIndex(displayedIndex -1);
+    }
+  };
+
+  const handleScrollRight = () => {
+    if (displayedIndex < images.length - 1) {
+      setDisplayedIndex(displayedIndex +1);
+    }
+  };
+
   const clearCategoryFilter = () => {
     setSelectedCategory(null);
   };
 
+  let displayedImages = images;
+  if (selectedCategory) {
+    displayedImages = images.filter(image => image.category === selectedCategory);
+  }
+
   return (
     <div style = {{ position: 'relative' }}>
       <div>
+        <button onClick={handleScrollLeft}>&lt;</button>
+        <button onClick={handleScrollRight}>&gt;</button>
         <button onClick = {() => filterImagesByCategory(null)}>All</button>
         {selectedCategory && <button onClick = {clearCategoryFilter}>Clear Filter</button>}
       </div>
 
       <img src = {baseImage} alt = "Avatar" />
 
-      {images.map((image, index) => {
-        if (!selectedCategory || image.category === selectedCategory) {
+      {displayedImages.map((image, index) => {
           return (
             <a key = {index} href = '#' onClick = {(e) => handleImageClick(e, image)}>
               <img
@@ -44,11 +64,9 @@ function ImageLayer(props) {
                   left: image.position.left,
                 }}
               />
+              <button onClick={() => handleAddImageToLayer(image)}>Add</button>
             </a>
           );
-        } else {
-          return null;
-        }
       })}
     </div>
   );
