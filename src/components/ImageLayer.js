64a74be67bb46799bff1a3115631fecbbc7811f3
Chloe ONeil <chloesporri@yahoo.com>
2023-07-25 14:57:39 -0700

    add selectedCategory func

--- src/components/ImageLayer.js
+++ src/components/ImageLayer.js
@@ -1,31 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ImageLayer(props) {
 
   const { images, baseImage } = props;
+  const [selectedCategory, setSelectedCategory] = useState(null);
 
   const handleImageClick = (e, image) => {
     console.log("Image clicked", image.src);
   };
 
+  const filterImagesByCategory = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleAddImageToLayer = (image) => {
+    console.log("Adding image to layer", image.src);
+  };
+
+  const clearCategoryFilter = () => {
+    setSelectedCategory(null);
+  };
+
   return (
     <div style = {{ position: 'relative' }}>
+      <div>
+        <button onClick = {() => filterImagesByCategory(null)}>All</button>
+        {selectedCategory && <button onClick = {clearCategoryFilter}>Clear Filter</button>}
+      </div>
+
       <img src = {baseImage} alt = "Avatar" />
 
-      {images.map((image, index) => (
-        <a key = {index} href = '#' onClick = {(e) => handleImageClick(e, image)}>
-          <img
-            key={index}
-            src={image.src}
-            alt={`{$index}`}
-            style={{
-              position: 'absolute',
-              top: image.position.top,
-              left: image.position.left,
-            }}
-          />
-        </a>
-      ))}
+      {images.map((image, index) => {
+        if (!selectedCategory || image.category === selectedCategory) {
+          return (
+            <a key = {index} href = '#' onClick = {(e) => handleImageClick(e, image)}>
+              <img
+                key={index}
+                src={image.src}
+                alt={`{$index}`}
+                style={{
+                  position: 'absolute',
+                  top: image.position.top,
+                  left: image.position.left,
+                }}
+              />
+            </a>
+          );
+        } else {
+          return null;
+        }
+      })}
     </div>
   );
 };
