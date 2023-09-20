5518078bf9551a5b99d6c2cd20cae63be6e08afe
Chloe ONeil <chloesporri@yahoo.com>
2023-09-20 11:12:29 -0700

    add LoadingScreen to components

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material';
+
+const LoadingScreen = () => {
+
+
+  return (
+    <Grid
+      sx={{
+        backgroundColor: 'green',
+        minHeight: '100%'
+      }}
+    />
+  );
+};
+
+export default LoadingScreen;
