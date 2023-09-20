7e3201ac9a9928930fe4879f59abf9f8465614c2
Chloe ONeil <chloesporri@yahoo.com>
2023-09-20 11:29:27 -0700

    move to articles

---
+++ src/components/article/ArticleDetail.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ArticleDetail(props) {
+  const { article, onClickingDelete, onClickingEdit } = props;
+  
+  return (
+    <React.Fragment>
+      <h1>Article's Detail</h1>
+      <h3>{article.articleName}</h3>
+      <h3>{article.lastWorn}</h3>
+      {article.image && <img src={URL.createObjectURL(article.image)} alt={article.articleName} />}
+      <h3>{article.category} - {article.occasion}</h3>
+      {Array.isArray(article.season) && <h3>{article.season.join(", ")}</h3>}
+      <button onClick = {() => onClickingEdit(article.id) }>Update Item</button>
+      <button onClick={() => onClickingDelete(article.id) }>Delete Item</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+ArticleDetail.propTypes = {
+  article: PropTypes.object,
+  onClickingEdit: PropTypes.func,
+  onClickingDelete: PropTypes.func
+};
+
+export default ArticleDetail;
